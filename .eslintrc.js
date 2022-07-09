18b8ff2ee890feb33797317c9905d0ff0a428757
IgnacioLF <100155212+IgnacioLF@users.noreply.github.com>
2022-07-09 23:29:55 +0200

    -added Navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: ["handlebarlabs"],
-  rules: {
-    "react/jsx-props-no-spreading": 0,
-    "react/jsx-curly-newline": 0,
-    "react/style-prop-object": 0,
-    "react/react-in-jsx-scope": 0,
-  },
+	extends: ["handlebarlabs"],
+	rules: {
+		"react/jsx-props-no-spreading": 0,
+		"react/jsx-curly-newline": 0,
+		"react/style-prop-object": 0,
+		"react/react-in-jsx-scope": 0,
+		"react/jsx-indent": "off",
+	},
 };
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,52 +1,54 @@
 import React from "react";
 import {
-  View,
-  TouchableOpacity,
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  Linking,
-  Alert,
+	View,
+	TouchableOpacity,
+	Text,
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	Linking,
+	Alert,
+	StatusBar,
 } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import colors from "../constants/colors";
 import { RowItem, RowSeparator } from "../components/RowItem";
 
 const openUrl = (url) => {
-  Linking.openURL(url).catch(() => {
-    Alert.alert("Sorry something went wrong.", "Please try again later.");
-  });
+	Linking.openURL(url).catch(() => {
+		Alert.alert("Sorry something went wrong.", "Please try again later.");
+	});
 };
 
 export default () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView>
-        <RowItem
-          onPress={() => openUrl("https://www.google.es")}
-          rightIcon={
-            <Entypo name="chevron-right" size={20} color={colors.blue} />
-          }
-          text="Themes"
-        />
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<ScrollView>
+				<StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+				<RowItem
+					onPress={() => openUrl("https://www.google.es")}
+					rightIcon={
+						<Entypo name="chevron-right" size={20} color={colors.blue} />
+					}
+					text="Themes"
+				/>
 
-        <RowSeparator />
+				<RowSeparator />
 
-        <RowItem
-          onPress={() => openUrl("https://www.google.es")}
-          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-          text="React Native Basics"
-        />
+				<RowItem
+					onPress={() => openUrl("https://www.google.es")}
+					rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+					text="React Native Basics"
+				/>
 
-        <RowSeparator />
+				<RowSeparator />
 
-        <RowItem
-          onPress={() => openUrl("https://www.google.es")}
-          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-          text="React Native by Example"
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
+				<RowItem
+					onPress={() => openUrl("https://www.google.es")}
+					rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+					text="React Native by Example"
+				/>
+			</ScrollView>
+		</SafeAreaView>
+	);
 };
